69e77e848e579d7103982ef68a792cf97818a568
HundredCleanWater <maxmp9904@gmail.com>
2021-11-08 19:35:40 +0900

    finish

--- src/main/java/question06.java
+++ src/main/java/question06.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class question06 {
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+
+        for(int i : numbers) { // for(int i = 0; i < numbers.length; i ++ (향상된 for문(for each문) / 자바뽀개기 10강 및 JAVA의 정석 p.166 참조)
+            answer -= i ;   //answer -= numbers[i] ;
+        }
+        return answer;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
