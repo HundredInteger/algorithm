acbfb7a12f14d2778bc39acef0718298c2639eb1
HundredCleanWater <maxmp9904@gmail.com>
2021-11-09 16:23:27 +0900

    finish

--- src/main/java/question10.java
+++ src/main/java/question10.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {
+    public int[][] solution(int[][] arr1, int[][] arr2) { // int[][] -> 다차원배열(테이블을 만들어줌)
 
+        //java의 정석 p.225~227 참조
+        final int ROW = arr1[0].length; // arr1과 arr2의 길이는 같기 때문에 arr1로만 설정해도 무관
+        final int COL = arr1.length; // 오후시간에 발표 -> 어차피 0행의 열길이나 1행의 열길이나 똑같이때문에 0으로 설정/ 근데 왜 하필 0이냐? 어떤 행렬에는 3열0행처럼 0행만 있는 경우도 있을꺼니깐!
 
-        final int ROW = arr1.length;
-        final int COL = arr1[0].length;
-
-        int [][]answer = new int[ROW][COL];
+        int [][]answer = new int[ROW][COL]; // ROW행 COL열을 가진 answer 2차원 배열 생성
         for(int i = 0 ; i < ROW ; i++){
             for(int j=0 ; j< COL ; j++){
 
-                answer[i][j] += arr1[i][j] + arr2[i][j];
-
+                answer[i][j] += arr1[i][j] + arr2[i][j]; //answer의 i행j열은 arr1의 i행j열과 arr2의 i행j열의 더한 값이다.
 
             }
         }
         return answer;
     }
 }
+
--- src/main/java/question14.java
+++ src/main/java/question14.java
@@ -1,32 +1,61 @@
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
     public int[] solution(int[] arr, int divisor) {
-        ArrayList<Integer> a = new ArrayList<Integer>();
+        ArrayList<Integer> a = new ArrayList<Integer>(); // divisor로 나누어 떨어지는 값 a에 추가
+        //ArrayList : Collections클래스의 sort메서드를 이용하여 저장된 객체들을 정렬가능.
         int[] answer = {};
 
-        for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i] % divisor = 0) {
-                a.add(arr[i]); // arr의 요소들을 저장
-            } if else (a.isEmpty()){
-                a.add(-1);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                a.add(arr[i]); // 만들어진 ArrayLisy에 arr의 값들을 저장
             }
-            answer = new int[a.size()];
-            for(int i = 0; i < a.size(); i++){
-                answer[i] = a.get(i);
-            }
-            Arrays.sort(answer);
-            return answer;
         }
-
+        answer = new int[a.size()];  // a의 size( a 에 들어있는 수수만큼 반환할 배열 answer 생성)-> 현재 a에 ~~~만큼 들어있다.
+        if (a.size() == 0) { //divisor로 나누어지는 값 없으면-> a에 아무것도 없으면
+            answer = new int[1]; //  -1 들어갈 배열 생성
+            answer[0] = -1; // 배열에 -1 추가
+        } else { // 나누어 떨어지는 값이 있으면
+            for (int i = 0; i < a.size(); i++) { // a의 길이만큼 answer에 a의 값 추가
+                answer[i] = a.get(i); //ArrayList a에 있는 값들을 하나씩 꺼내서 answer에 넣어 줌.
+            }
+        }
+        Arrays.sort(answer); // ArrayList의 결과를 출력하는 방법/Arrays.sort(arr,Collections.reverseOrder()); : 내림차순
+        return answer;
     }
 }
 
 
+/*        class Divisible {
+            public int[] divisible(int[] array, int divisor) {
+                //ret에 array에 포함된 정수중, divisor로 나누어 떨어지는 숫자를 순서대로 넣으세요.
+                return Arrays.stream(array).filter(factor -> factor % divisor == 0).toArray(); //->Arrays.Stream() API
+            }*/
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+        //Q1. 왜 ArrayList를 사용하는가?
+        //A1. Collections클래스의 sort메서드를 이용하여 오름차순 정렬을 위해 사용(다른 이유가 더 있을수도 있을 것 같다.)
+
+        //Q2.왜 length대신 size를 사용했냐?
+        //A2. array(배열)에서 길이를 나타내주는 건 length arrayList(배열리스트)에서 길이를 나타내 주는 건 size
+
+        //Q3. add와 get을 쓰는 이유
+        //A3. add는 ArrayList에서 추가 할 때 사용을 하고 get은 꺼낼 때 사용 한다.
+
+        //
 
