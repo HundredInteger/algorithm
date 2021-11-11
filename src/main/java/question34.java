4cd201b5e10e102d622594ecad8f9f08a3a3cca2
HundredCleanWater <maxmp9904@gmail.com>
2021-11-11 17:33:56 +0900

    first x

---
+++ src/main/java/question34.java
@@ -0,0 +1,45 @@
+/*
+1번친구 : 1,2,3,4,5 순서대로 (for구문써서 배열의길이만큼 반복?)
+2번친구 : 짝수번째 2 홀수번째 1,3,4,5(2제외)
+3번친구 : 3,3,1,1,4,4,2,2,5,5 노답
+-> 배열에 반복되는 숫자들을 적어놓고, if문을 사용하여 나눈 후 나머지값을 이용하면 될듯?
+ */
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+class Solution {
+    public int[] solution(int[]answers) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        int[] one = {1,2,3,4,5};
+        int[] two = {2,1,2,3,2,4,2,5};
+        int[] three = {3,3,1,1,4,4,2,2,5,5};
+        int[] scores = {0,0,0};
+
+        for (int i = 0 ; i<answers.length  ; i++){
+            if(one[i % 5] ==answers[i]){scores[0]++;}
+            if(two[i % 8] ==answers[i]){scores[1]++;}
+            if(two[i % 10] ==answers[i]){scores[2]++;}
+
+        }
+        int[] arr = new int[scores.length];
+        for(int i = 0; i < scores.length ; i++){
+            arr[i] = scores[i];
+        }
+        Arrays.sort(arr);
+
+        for(int i = 0 ; i < arr.length; i++){
+            if(scores[i] == arr[2]){
+                list.add(i+1);
+            }
+        }
+        int[] answer = new int[list.size()];
+        for(int i = 0 ; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
