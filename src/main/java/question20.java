da8e9006c499d2d34869e04d22a77173ff3b1c40
HundredCleanWater <maxmp9904@gmail.com>
2021-11-12 20:53:29 +0900

    finish

---
+++ src/main/java/question20.java
@@ -0,0 +1,19 @@
+import java.util.*; // java.util.Arrays;만 해도 괜찮음
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+
+        int i;
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for ( i = 0; i < completion.length; i++ ) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+
+        return participant[i];
+    }
+}
+
