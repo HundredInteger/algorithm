0bf256299b871158cb9683c30d2099a66dc641ed
HundredCleanWater <90129613+HundredCleanWater@users.noreply.github.com>
2021-11-16 15:37:38 +0900

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,7 +1,7 @@
 class StringExercise{
     String getMiddle(String word){
 
-        return word.substring((word.length()-1) / 2, word.length()/2 + 1);
+        return word.substring((word.length()-2) / 2, word.length()/2 + 1);
     }
     // 아래는 테스트로 출력해 보기 위한 코드입니다.
     public static void  main(String[] args){
